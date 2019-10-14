939ca20701caa79f39736da9cd7358dba4de23b4
ZeroIce <1724264854@qq.com>
2019-10-14 22:25:51 +0800

    首页基本框架搭建

--- GabageProgramma/miniprogram/page/packageA/index/index.js
+++ GabageProgramma/miniprogram/page/packageA/index/index.js
@@ -5,7 +5,58 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 八大主题
+    thems: [
+      {
+        "color": "#2C3E50",
+        "image": "http://118.178.181.46/thems/1.jpg",
+        "logo": "../resources/images/1.png",
+        "url": "../../../packageF/pages/index/index"
+      },
+      {
+        "color": "#ff9900",
+        "image": "http://118.178.181.46/thems/2.jpg",
+        "logo": "../resources/images/2.png",
+        "url": "../../../packageG/pages/index/index"
+      },
+      {
+        "color": "#19be6b",
+        "image": "http://118.178.181.46/thems/3.jpg",
+        "logo": "../resources/images/3.png",
+        "url": "../../../packageE/pages/index/index"
+      },
+      {
+        "color": "#AEDD81",
+        "image": "http://118.178.181.46/thems/4.jpg",
+        "logo": "../resources/images/4.png",
+        "url": "../../../packageH/pages/index/index"
+      },
+      {
+        "color": "#84AF9B",
+        "image": "http://118.178.181.46/thems/5.jpg",
+        "logo": "../resources/images/5.png",
+        "url": "../../../packageI/pages/index/index"
+      },
+      {
+        "color": " #2d8cf0",
+        "image": "http://118.178.181.46/thems/6.jpg",
+        "logo": "../resources/images/6.png",
+        "url": "../../../packageJ/pages/index/index"
+      },
+      {
+        "color": "#1c2438",
+        "image": "http://118.178.181.46/thems/7.jpg",
+        "logo": "../resources/images/7.png",
+        "url": "../../../packageK/pages/index/index"
+      },
+      {
+        "color": "#FC9D99",
+        "image": "http://118.178.181.46/thems/8.jpg",
+        "logo": "../resources/images/8.png",
+        "url": "../../../packageL/pages/index/index"
+      }
+    ],
+    currentImg: 0
   },
 
   /**
@@ -14,53 +65,14 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  themsChange: function (e) {
+    let _this = this
+    console.log(e.detail.current)
+    _this.setData({
+      currentImg: e.detail.current
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  locationChange:function(e){
+    console.log(e)
   }
 })
